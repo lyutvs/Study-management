c35f7607e142a095cd79310b7d9f4362ac5dc3ef
sehyoun <lyutvs@gmail.com>
2021-09-23 22:47:30 +0900

    ⚡️ :: 아이디 생성

--- src/main/java/com/example/study_webapp/dao/CommonDao.java
+++ src/main/java/com/example/study_webapp/dao/CommonDao.java
@@ -1,2 +1,21 @@
-package com.example.study_webapp.dao;public class CommonDao {
+package com.example.study_webapp.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository("commonDao")
+public class CommonDao {
+
+    @Autowired
+    private SqlSessionTemplate sqlSession;
+
+    //protected void printQueryId(String queryId){
+    //	if(log.isDebugEnabled()){ log.debug("\t QueryId \t: " + queryId); } }
+
+    public Object insert(String queryId, Object params){
+        //printQueryId(queryId);
+        return sqlSession.insert(queryId, params);
+    }
+
 }
