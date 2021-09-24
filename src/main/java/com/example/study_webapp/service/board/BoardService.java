66225552cc1785b4ab69558004204628bba3b65c
sehyoun <lyutvs@gmail.com>
2021-09-24 22:50:05 +0900

    ⚡️ :: 기능 구현

--- src/main/java/com/example/study_webapp/service/board/BoardService.java
+++ src/main/java/com/example/study_webapp/service/board/BoardService.java
@@ -1,2 +1,28 @@
-package com.example.study_webapp.service.board;public class BoardService {
+package com.example.study_webapp.service.board;
+
+import java.util.List;
+import java.util.Map;
+
+public interface BoardService {
+
+    List<Map<String,Object>> selectBoard(Map<String, Object> param) throws Exception;
+
+    Map<String,Object> selectDetail(Map<String, Object> param) throws Exception;
+
+    Integer insertBoard(Map<String, Object> param) throws Exception;
+
+    void updateCount(Map<String, Object> param) throws Exception;
+
+    void updateBoard(Map<String, Object> param) throws Exception;
+
+    void deleteBoard(Map<String, Object> param) throws Exception;
+
+    Map<String, Object> selectMaxIdx()throws Exception;
+
+    List<Map<String,Object>> serchBoard(Map<String, Object> param)throws Exception;
+
+    List<Map<String,Object>>selectLastComment() throws Exception;
+
+    List<Map<String, Object>> selectCategory()throws Exception;
+
 }
