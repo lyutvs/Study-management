97b71042b006443214fed7bdf814cd8597f71138
sehyoun <lyutvs@gmail.com>
2021-09-24 22:58:14 +0900

    ⚡️ :: Index 생성

--- src/main/java/com/example/study_webapp/model/util/PagingUtil.java
+++ src/main/java/com/example/study_webapp/model/util/PagingUtil.java
@@ -1,2 +1,18 @@
-package com.example.study_webapp.model.util;public class PagingUtil {
+package com.example.study_webapp.model.util;
+
+import java.util.List;
+
+public class PagingUtil {
+
+    private int pageIndex;
+
+    private int startRow;
+
+    private int pageSize;
+
+    private List<?> resultList;
+
+    private int totalCount;
+
+
 }
