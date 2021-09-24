f15e4d03bb7c98858209e54d10ca49fc95d8cf29
sehyoun <lyutvs@gmail.com>
2021-09-24 16:35:23 +0900

    ⚡️ :: 유저 회원 가입

--- src/main/java/com/example/study_webapp/service/UserJoinServiceImpl.java
+++ src/main/java/com/example/study_webapp/service/UserJoinServiceImpl.java
@@ -1,14 +1,28 @@
 package com.example.study_webapp.service;
 
+import com.example.study_webapp.dao.CommonDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 @Service("UserJoinService")
 public class UserJoinServiceImpl implements UserJoinService {
-    
+
+    @Autowired
+    private CommonDao commonDao;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     @Override
     public Object insertUser(Map<String, Object> param) throws Exception {
+
+        String password = (String) param.get("password");
+        param.put("password", passwordEncoder.encode(password));
+
+        commonDao.insert("user.insertUser", param);
         return null;
     }
 
