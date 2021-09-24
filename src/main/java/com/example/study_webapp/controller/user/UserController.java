529e06b5fe23bf78af1ca4a5446215acd07e7b94
sehyoun <lyutvs@gmail.com>
2021-09-24 22:36:06 +0900

    gitflow-feature-stash: board

---
+++ src/main/java/com/example/study_webapp/controller/user/UserController.java
@@ -0,0 +1,14 @@
+package com.example.study_webapp.controller.user;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class UserController {
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
+}
