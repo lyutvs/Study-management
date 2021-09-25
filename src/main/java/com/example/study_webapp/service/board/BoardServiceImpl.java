e48b314805d84065251290c19b647f73b367810b
sehyoun <lyutvs@gmail.com>
2021-09-25 15:49:33 +0900

    ⚡️ :: selectDetail

--- src/main/java/com/example/study_webapp/service/board/BoardServiceImpl.java
+++ src/main/java/com/example/study_webapp/service/board/BoardServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.study_webapp.service.board;
 
+import com.example.study_webapp.dao.CommonDao;
+import com.example.study_webapp.model.util.PagingUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,15 +11,28 @@ import java.util.Map;
 @Service("boardService")
 public class BoardServiceImpl implements BoardService{
 
+    @Autowired
+    private CommonDao commonDao;
 
     @Override
     public List<Map<String, Object>> selectBoard(Map<String, Object> param) throws Exception {
-        return null;
+        PagingUtil paging = new PagingUtil();
+        int index = 1;
+        if(param.containsKey("pageIndex")) {
+            index = Integer.parseInt(param.get("pageIndex").toString());
+            paging.setPageIndex(index);
+
+        }
+        paging.countPaging(index, 5);
+        param.put("page",paging);
+
+        return commonDao.selectList("board.selectBoard", param);
     }
 
     @Override
     public Map<String, Object> selectDetail(Map<String, Object> param) throws Exception {
-        return null;
+
+        return commonDao.selectOne("board.detailBoard",param);
     }
 
     @Override
