f7d35f3e792d192d60360c198410954c4f08b23e
sehyoun <lyutvs@gmail.com>
2021-09-25 23:24:43 +0900

    ⚡️ :: PasswordConfig

--- src/main/java/com/example/study_webapp/config/security/PasswordConfig.java
+++ src/main/java/com/example/study_webapp/config/security/PasswordConfig.java
@@ -1,4 +1,4 @@
-package com.example.study_webapp.config;
+package com.example.study_webapp.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
