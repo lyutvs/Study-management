f01b1349e14cffa374ccd8381061a60b206bcc93
sehyoun <lyutvs@gmail.com>
2021-09-25 15:44:59 +0900

    ⚡️ :: 메소드 추가

--- src/main/java/com/example/study_webapp/model/util/PagingUtil.java
+++ src/main/java/com/example/study_webapp/model/util/PagingUtil.java
@@ -8,11 +8,52 @@ public class PagingUtil {
 
     private int startRow;
 
+    private int endRow;
+
     private int pageSize;
 
     private List<?> resultList;
 
     private int totalCount;
 
+    public int getPageIndex() {
+        return pageIndex;
+    }
 
+    public int getStartRow() {
+        return startRow;
+    }
+
+    public int getEndRow() {
+        return endRow;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public List<?> getResultList() {
+        return resultList;
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public void setPageIndex(int pageIndex) {
+        this.pageIndex = pageIndex;
+    }
+
+    public void countPaging(int pageIndex, int pageSize) {
+        this.pageIndex = this.pageIndex < 1 ? 1 : this.pageIndex;
+        this.pageSize = pageSize;
+        this.startRow = (pageIndex-1)*pageSize;
+        this.endRow = startRow + pageSize;
+    }
+
+    public void countPagingList(List<?>resultList, int totalCount) {
+        this.resultList = resultList;
+        this.totalCount = totalCount;
+
+    }
 }
