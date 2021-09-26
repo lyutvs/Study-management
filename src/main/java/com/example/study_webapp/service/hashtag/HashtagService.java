598316b28b69dfea6bb7c74c1f709a87b83c7631
sehyoun <lyutvs@gmail.com>
2021-09-26 14:26:35 +0900

    ⚡️ :: 이름 변경 및 해쉬태그 모두 선택

--- src/main/java/com/example/study_webapp/service/hashtag/HashtagService.java
+++ src/main/java/com/example/study_webapp/service/hashtag/HashtagService.java
@@ -1,14 +1,19 @@
 package com.example.study_webapp.service.hashtag;
 
+import java.util.HashSet;
 import java.util.Map;
 
-public interface HashTagService {
+public interface HashtagService {
 
     void insertHashTag(Map<String, Object> param) throws Exception;
 
     Map<String, Object> selectOneHashTag(Map<String, Object> param) throws Exception;
 
     void updateHashTag(Map<String, Object> param) throws Exception;
+
+    HashSet<String> selectHashTag(Map<String, Object> param) throws Exception;
+
+    HashSet<String> selectAllHashTag() throws Exception;
 
 
 
