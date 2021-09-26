eeb84f67a90300e31d61fc4e1dd320147192bbbb
sehyoun <lyutvs@gmail.com>
2021-09-27 08:39:51 +0900

    ⚡️ :: NoticeService Impl 해줌

--- src/main/java/com/example/study_webapp/service/notice/NoticeServiceImpl.java
+++ src/main/java/com/example/study_webapp/service/notice/NoticeServiceImpl.java
@@ -1,2 +1,55 @@
-package com.example.study_webapp.service.notice;public class NoticeServiceImpl {
+package com.example.study_webapp.service.notice;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service("NoticeService")
+public class NoticeServiceImpl implements NoticeService{
+    
+    @Override
+    public List<Map<String, Object>> selectBoard(Map<String, Object> param) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> selectDetail(Map<String, Object> param) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Object insertBoard(Map<String, Object> param) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void updateCount(Map<String, Object> param) throws Exception {
+
+    }
+
+    @Override
+    public void updateBoard(Map<String, Object> param) throws Exception {
+
+    }
+
+    @Override
+    public void deleteBoard(Map<String, Object> param) throws Exception {
+
+    }
+
+    @Override
+    public Map<String, Object> selectMaxIdx() throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<Map<String, Object>> serchBoard(Map<String, Object> param) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> selectCount(Map<String, Object> param) throws Exception {
+        return null;
+    }
 }
