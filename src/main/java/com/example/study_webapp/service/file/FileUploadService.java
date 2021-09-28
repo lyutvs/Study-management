0476334b004bcae678e06286f3fe3bad23061b5e
sehyoun <lyutvs@gmail.com>
2021-09-28 21:57:56 +0900

    ⚡️ :: FileUploadService

--- src/main/java/com/example/study_webapp/service/file/FileUploadService.java
+++ src/main/java/com/example/study_webapp/service/file/FileUploadService.java
@@ -4,22 +4,22 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import java.lang.Exception;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public interface FileUploadService {
 
-    Map<String, Object> selectGroup() throws IOException;
+    Map<String, Object> selectGroup() throws Exception;
 
-    Map<String, Object> insertAttachFile(List<MultipartFile> fileList, Map<String, Object> param) throws IOException;
+    Map<String, Object> insertAttachFile(List<MultipartFile> fileList, Map<String, Object> param) throws Exception;
 
-    List<Map<String,Object>> selectAttachFileListByIDX(Map<String, Object> param) throws IOException;
+    List<Map<String,Object>> selectAttachFileListByIDX(Map<String, Object> param) throws Exception;
 
-    List<Map<String, Object>> selectAttachFileListByGroup(Map<String, Object> param) throws IOException;
+    List<Map<String, Object>> selectAttachFileListByGroup(Map<String, Object> param) throws Exception;
 
-    void deleteFileAttach(Map<String, Object> param) throws IOException;
+    void deleteFileAttach(Map<String, Object> param) throws Exception;
 
     void selectAttachFileDownload(HttpServletResponse request, HttpServletResponse response, Map<String, Object> param)throws Exception;
 
