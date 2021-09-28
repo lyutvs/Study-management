b8d22442960940618c224575de2a4e96de16a170
sehyoun <lyutvs@gmail.com>
2021-09-28 19:29:31 +0900

    ⚡️ :: 필요 하니까 먼저 만들어놈 인덱스 선택

--- src/main/java/com/example/study_webapp/service/file/FileUploadService.java
+++ src/main/java/com/example/study_webapp/service/file/FileUploadService.java
@@ -1,2 +1,15 @@
-package com.example.study_webapp.service.file;public class fileUploadService {
+package com.example.study_webapp.service.file;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public interface FileUploadService {
+
+
+    List<Map<String,Object>> selectAttachFileListByIDX(Map<String, Object> param) throws Exception;
+
+
 }
