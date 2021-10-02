d346e848c23e2f54f18c90c7c851daba9bd03a5e
세현 <lyutvs@gmail.com>
2021-10-02 23:14:42 +0900

    ⚡️ :: 보드 뷰 컨트롤러
    @ResponseStatus(HttpStatus.) 이거 쓰고 아래 메소드는 뺴야 한다

--- src/main/java/com/example/study_webapp/controller/board/BoardController.java
+++ src/main/java/com/example/study_webapp/controller/board/BoardController.java
@@ -5,6 +5,13 @@ import com.example.study_webapp.service.file.FileUploadService;
 import com.example.study_webapp.service.hashtag.HashtagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class BoardController {
@@ -15,5 +22,42 @@ public class BoardController {
     private FileUploadService fileuploadService;
     @Autowired
     private HashtagService hashtagService;
+
+
+
+
+    @RequestMapping(value="/board/{idx}" , method = {RequestMethod.GET})
+    @ResponseBody
+    public ModelAndView selectDetail(HttpServletRequest request,
+                                     HttpServletResponse response,
+                                     @RequestParam Map<String,Object> param,
+                                     @PathVariable int idx) throws Exception {
+
+        ModelAndView mv = new ModelAndView("board/boardVw.tiles");
+
+        param.put("IDX", idx);
+        mv.addObject("param", param);
+        mv.addObject("list", boardService.selectBoard(param));
+        mv.addObject("data", boardService.selectDetail(param));
+
+        List<Map<String, Object>> list = fileuploadService.selectAttachFileListByIDX(param);
+
+        if(!list.isEmpty() || list != null ) {
+            mv.addObject("files", list);
+        }
+
+        //해시태그 obj
+        Map<String,Object> hash = hashtagService.selectOneHashTag(param);
+        if(hash != null) {
+            if(hash.size() > 0) {
+                String[] arr = ((String) hash.get("CONTENTS")).split(",");
+                mv.addObject("hash", arr);
+            }
+        }
+
+        boardService.updateCount(param);
+        return mv;
+    }
+
 
 }
