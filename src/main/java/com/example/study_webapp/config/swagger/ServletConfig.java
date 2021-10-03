dad4b2f7f94028d3464d3ae4192fe38f45c986de
세현 <lyutvs@gmail.com>
2021-10-03 19:48:19 +0900

    ⚡️ :: 리소스 등록 및 핸들러를 관리하는 객체

--- src/main/java/com/example/study_webapp/config/swagger/ServletConfig.java
+++ src/main/java/com/example/study_webapp/config/swagger/ServletConfig.java
@@ -1,2 +1,22 @@
-package com.example.study_webapp.config.swagger;public class ServletConfig {
+package com.example.study_webapp.config.swagger;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@ComponentScan(basePackages= {"com.example.study_management"})
+public class ServletConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+    }
+
+
 }
