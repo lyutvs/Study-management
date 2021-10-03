c60831b80d1c66dac943ce75c9de4fd80db578c6
세현 <lyutvs@gmail.com>
2021-10-03 18:10:33 +0900

    ⚡️ :: Mysql 연결 빈

--- src/main/java/com/example/study_webapp/config/database/DatabaseConfig.java
+++ src/main/java/com/example/study_webapp/config/database/DatabaseConfig.java
@@ -1,2 +1,45 @@
-package com.example.study_webapp.config.database;public class DatabaseConfig {
+package com.example.study_webapp.config.database;
+
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Value("${database.mysql.classname}")
+    private String classname;
+
+    @Value("${database.mysql.url}")
+    private String url;
+
+    @Value("${database.mysql.username}")
+    private String username;
+
+    @Value("${database.mysql.password}")
+    private String password;
+
+
+    @Bean
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource source = new DriverManagerDataSource();
+        source.setDriverClassName(classname);
+        // 자바 설정으로 쓸 때에는 url에 &을 &amp; 로 치환하면 에러남
+        source.setUrl(url);
+        source.setUsername(username);
+        source.setPassword(password);
+
+        return source;
+    }
+
 }
