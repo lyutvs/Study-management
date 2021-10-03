1257e74f701a554586effc78a8a8fb392e808f03
세현 <lyutvs@gmail.com>
2021-10-03 18:17:28 +0900

    ⚡️ :: Redis 연동

--- src/main/java/com/example/study_webapp/config/database/RedisConfig.java
+++ src/main/java/com/example/study_webapp/config/database/RedisConfig.java
@@ -1,2 +1,20 @@
-package com.example.study_webapp.config.database;public class RedisConfig {
+package com.example.study_webapp.config.database;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory("127.0.0.1", 6379);
+    }
+
+    
 }
