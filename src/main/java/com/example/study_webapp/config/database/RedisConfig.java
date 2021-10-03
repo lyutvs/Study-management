d7733157222d12a694f6750ad4fd11c038461999
세현 <lyutvs@gmail.com>
2021-10-03 18:22:20 +0900

    ⚡️ :: Redis Command 를 도와주는 Template

--- src/main/java/com/example/study_webapp/config/database/RedisConfig.java
+++ src/main/java/com/example/study_webapp/config/database/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
@@ -16,5 +17,12 @@ public class RedisConfig {
         return new LettuceConnectionFactory("127.0.0.1", 6379);
     }
 
-    
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+
+
 }
