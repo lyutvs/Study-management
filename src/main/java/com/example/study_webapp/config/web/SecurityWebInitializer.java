806bba1e0f2d4d129a7d647312506b7814d22177
세현 <lyutvs@gmail.com>
2021-10-05 08:36:08 +0900

    📑 :: 이름 바꿈

--- src/main/java/com/example/study_webapp/config/web/SecurityWebInitializer.java
+++ src/main/java/com/example/study_webapp/config/web/SecurityWebInitializer.java
@@ -3,6 +3,6 @@ package com.example.study_webapp.config.web;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
-public class AppSecurityConfig extends AbstractSecurityWebApplicationInitializer {
+public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
 
 }
