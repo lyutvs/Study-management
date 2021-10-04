b63181c556dc918370b75ddb3aa7e7da9764d01b
세현 <lyutvs@gmail.com>
2021-10-04 21:43:16 +0900

    ⚡️ :: 반복적인 코드 합하는 빈

--- src/main/java/com/example/study_webapp/config/swagger/ServletConfig.java
+++ src/main/java/com/example/study_webapp/config/swagger/ServletConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesView;
 
 @EnableWebMvc
@@ -27,6 +28,13 @@ public class ServletConfig implements WebMvcConfigurer {
         urlBasedViewResolver.setViewClass(TilesView.class);
         urlBasedViewResolver.setOrder(1);
         return urlBasedViewResolver;
+    }
+
+    @Bean
+    public TilesConfigurer tilesConfigurer() {
+        TilesConfigurer tilesConfigurer = new TilesConfigurer();
+        tilesConfigurer.setDefinitions("/WEB-INF/views/layout/tiles-layout.xml");
+        return tilesConfigurer;
     }
 
 }
