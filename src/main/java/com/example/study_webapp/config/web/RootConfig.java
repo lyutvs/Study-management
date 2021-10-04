81d0c9b82bddd19e826f0ab940b82fc77dd629a8
세현 <lyutvs@gmail.com>
2021-10-05 08:32:55 +0900

    ⚡ :: XML 파일 대신에

--- src/main/java/com/example/study_webapp/config/web/RootConfig.java
+++ src/main/java/com/example/study_webapp/config/web/RootConfig.java
@@ -1,4 +1,11 @@
 package com.example.study_webapp.config.web;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages= {"com.example.study_webapp"})
 public class RootConfig {
+
+    
 }
