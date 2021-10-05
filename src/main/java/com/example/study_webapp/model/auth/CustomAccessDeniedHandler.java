88554297b5f69cd472bd67f3c66608eb9b3e1110
세현 <lyutvs@gmail.com>
2021-10-05 12:06:11 +0900

    ⚡️ :: CustonAccessDeniedHandler

--- src/main/java/com/example/study_webapp/model/auth/CustomAccessDeniedHandler.java
+++ src/main/java/com/example/study_webapp/model/auth/CustomAccessDeniedHandler.java
@@ -1,2 +1,35 @@
-package com.example.study_webapp.model.auth;public class CustomAccessDeniedHandler {
+package com.example.study_webapp.model.auth;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException e) throws IOException, ServletException {
+
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+
+        if (e instanceof AccessDeniedException) {
+
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            if (authentication != null) {
+
+                int error = HttpStatus.FORBIDDEN.value();
+                String errorPath = "/error" + error;
+
+                response.sendRedirect(request.getContextPath() + errorPath);
+
+            }
+        }
+    }
+
 }
