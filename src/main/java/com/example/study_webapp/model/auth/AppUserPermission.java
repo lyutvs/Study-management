fa175150ebc9223d95ea3dd7f70c6fe7e7f36f0e
세현 <lyutvs@gmail.com>
2021-10-05 11:44:56 +0900

    ⚡️ :: AppUserPermission

--- src/main/java/com/example/study_webapp/model/auth/AppUserPermission.java
+++ src/main/java/com/example/study_webapp/model/auth/AppUserPermission.java
@@ -1,2 +1,20 @@
-package com.example.study_webapp.model.auth;public enum AppUserPermission {
+package com.example.study_webapp.model.auth;
+
+public enum AppUserPermission {
+
+    USER_READ("user:read"),
+    USER_WRITE("user:write"),
+    GUEST_READ("guest:read");
+
+    private final String permission;
+
+    AppUserPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+
 }
