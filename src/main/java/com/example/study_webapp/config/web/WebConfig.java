bd5e4aebf1f88b18095d92ce19e9d4e55d0044a6
세현 <lyutvs@gmail.com>
2021-10-05 11:44:43 +0900

    ⚡️ :: WebConfig

--- src/main/java/com/example/study_webapp/config/web/WebConfig.java
+++ src/main/java/com/example/study_webapp/config/web/WebConfig.java
@@ -1,2 +1,28 @@
-package com.example.study_webapp.config.web;public class WebConfig {
+package com.example.study_webapp.config.web;
+
+import com.example.study_webapp.config.database.DatabaseConfig;
+import com.example.study_webapp.config.swagger.ServletConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+@Configuration
+@PropertySource("classpath:/applicaiton.properties")
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{RootConfig.class, SecurityWebInitializer.class, DatabaseConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{ServletConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
 }
