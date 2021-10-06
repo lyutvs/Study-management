6af2a26eb4f3b63a05885d4c21c2010bbecc39cc
세현 <lyutvs@gmail.com>
2021-10-06 18:44:44 +0900

    ⚡ :: JwtAuthenticationRequest

--- src/main/java/com/example/study_webapp/model/jwt/JwtAuthenticationRequest.java
+++ src/main/java/com/example/study_webapp/model/jwt/JwtAuthenticationRequest.java
@@ -1,2 +1,34 @@
-package com.example.study_webapp.model.jwt;public class JwtAuthenticationRequest {
+package com.example.study_webapp.model.jwt;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class JwtAuthenticationRequest {
+
+    private String username;
+    private String password;
+
+    @JsonCreator
+    public JwtAuthenticationRequest(@JsonProperty("username")String username, @JsonProperty("password") String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
 }
